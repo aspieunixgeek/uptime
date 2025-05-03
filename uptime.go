3be9bd4196f4b8b70f1a5a282b4b1fee2d4be558
aspieunixgeek <asperger.unixgeek@gmail.com>
2025-05-03 23:33:38 +0500

    fix

--- uptime.go
+++ uptime.go
@@ -48,7 +48,7 @@ func Uptime() (ut Up, err error) {
 	}
 
 	ut.CurTime = curTime.Format("15:04:05")
-	ut.Up = x[Hours] + " " + x[Min]
+	ut.Up = "up " + x[Hours] + " " + x[Min]
 	ut.User = ss[User]
 
 	trd := strings.TrimSpace(ss[2])
